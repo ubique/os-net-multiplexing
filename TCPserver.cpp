90de66a19ae6fbc891c2493bad6c7e06da045b2e
Domonion <artemkononov2@gmail.com>
2019-06-09 00:46:52 +0300

    minor fixes

--- TCPserver.cpp
+++ TCPserver.cpp
@@ -12,11 +12,30 @@
 #include <string>
 #include <vector>
 #include <utils.hpp>
+#include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <set>
 
+using std::set;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
+
+int set_nonblock(int fd) {
+    int flags;
+#if defined(O_NONBLOCK)
+    if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+        flags = 0;
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+#else
+    flags = 1;
+    return ioctl(fd, FIOBIO, &flags);
+#endif
+}
+
+const int MAX_EVENTS = 10;
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -32,20 +51,39 @@ int main(int argc, char **argv) {
     check_error(inet_pton(AF_INET, argv[1], &server.sin_addr), "inet_pton");
     check_error(bind(master, (sockaddr *) (&server), size), "bind");
     check_error(listen(master, SOMAXCONN), "listen");
+    int EPoll = epoll_create1(0);
+    check_error(EPoll, "epoll_create1");
+    struct epoll_event Event;
+    Event.data.fd = master;
+    Event.events = EPOLLIN;
+    check_error(epoll_ctl(EPoll, EPOLL_CTL_ADD, master, &Event), "epoll_ctl");
     while (true) {
-        int slave = accept(master, (sockaddr *) (&client), &size);
-        check_error(slave, "accept");
-        char size;
-        doRecv(&size, 1, slave);
-        vector<char> data(size);
-        doRecv(&data[0], size, slave);
-        cout << &data[0] << endl;
-        for (char &i : data) {
-            i += abs('A' - 'a');
+        struct epoll_event Events[MAX_EVENTS];
+        int N = epoll_wait(EPoll, Events, MAX_EVENTS, -1);
+        check_error(N, "epoll_wait");
+        for (int i = 0; i < N; i++) {
+            if (Events[i].data.fd == master) {
+                int slave = accept(master, (sockaddr *) (&client), &size);
+                check_error(slave, "accept");
+                struct epoll_event slaveEvent;
+                slaveEvent.data.fd = slave;
+                slaveEvent.events = EPOLLIN;
+                check_error(epoll_ctl(EPoll, EPOLL_CTL_ADD, slave, &slaveEvent), "epoll_ctl");
+            } else {
+                int slave = Events[i].data.fd;
+                char size;
+                doRecv(&size, 1, slave);
+                vector<char> data(size);
+                doRecv(&data[0], size, slave);
+                cout << &data[0] << endl;
+                for (char &i : data) {
+                    i += abs('A' - 'a');
+                }
+                data.back() = '\0';
+                doSend(&data[0], size, slave);
+                check_error(shutdown(slave, SHUT_RDWR), "shutdown");
+                check_error(close(slave), "close");
+            }
         }
-        data.back() = '\0';
-        doSend(&data[0], size, slave);
-        check_error(shutdown(slave, SHUT_RDWR), "shutdown");
-        check_error(close(slave), "close");
     }
 }
